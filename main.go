cb6dae36e78980d3d1249a9f2078844470038ae9
David Stark <david@starkers.org>
2023-10-01 20:31:42 +0100

    using custom header for remote IP

--- main.go
+++ main.go
@@ -1,65 +1,41 @@
 package main
 
 import (
-	"errors"
-	"fmt"
-	"net"
 	"net/http"
 
 	"github.com/hardmacro/outboundip/logger"
-	"github.com/labstack/echo-contrib/echoprometheus"
 	"github.com/labstack/echo/v4"
-	flag "github.com/spf13/pflag"
 )
 
-var flagDebug bool
-var flagIpFromXFF bool
-var flagListenPort string
-
-func init() {
-	flag.BoolVar(&flagDebug, "debug", false, "log in debug mode")
-	flag.BoolVar(&flagIpFromXFF, "ip-from-xff", false, "get all remote IPs from 'X-Forwarded-For' header")
-	flag.StringVar(&flagListenPort, "listen-port", "8080", "port to listen to for web traffic")
-}
 func main() {
-	flag.Parse()
-	logger.InitLogger(flagDebug)
+	logger.InitLogger(false)
 
-	logger.Infow("startup")
+	logger.Info("hello")
 
-	app := echo.New()
-	app.HideBanner = true
-	app.HidePort = true
+	e := echo.New()
 
-	if flagIpFromXFF {
-		_, ipV4, _ := net.ParseCIDR("0.0.0.0/0")
-		_, ipV6, _ := net.ParseCIDR("0:0:0:0:0:0:0:0/0")
-		app.IPExtractor = echo.ExtractIPFromXFFHeader(echo.TrustIPRange(ipV4), echo.TrustIPRange(ipV6))
-	}
+	e.GET("/", func(c echo.Context) error {
 
-	// metrics
-	// TODO: potential for high entropy as it records paths for 404's (and other) codes leaving it open to abuse
-	//       use a custom skipper and maybe just count 404's indivudually
-	app.Use(echoprometheus.NewMiddleware("outboundip"))
-	go func() {
-		metrics := echo.New()
-		metrics.HideBanner = true
-		metrics.HidePort = true
-		metrics.GET("/metrics", echoprometheus.NewHandler())
-		if err := metrics.Start(":9090"); err != nil && !errors.Is(err, http.ErrServerClosed) {
-			logger.Fatalw("startup failure", "error", err)
+		ip := "unknown"
+		for key, values := range c.Request().Header {
+			if key == "Do-Connecting-Ip" {
+				if len(values) > 0 {
+					ip = values[0]
+				}
+			}
+			// fmt.Printf(":%s = (", key)
+			// for _, value := range values {
+			// 	fmt.Printf(" ")
+			// 	fmt.Printf("%s", value)
+			// }
+			// fmt.Printf(")\n")
 		}
-	}()
+		// return nil
 
-	app.GET("/", func(c echo.Context) error {
-		logger.Infow("get",
-			"path", "/",
-			"remote", c.RealIP(),
-			"userAgent", c.Request().UserAgent(),
-		)
-		return c.String(http.StatusOK, c.RealIP())
+		logger.Infow(c.Request().RemoteAddr)
+		// ip := strings.Split(c.Request().RemoteAddr, ":")[0] // TODO: probably actually the real IP is a header?
+		logger.Info(ip)
+		return c.String(http.StatusOK, ip)
 	})
-	app.Logger.Fatal(
-		app.Start(fmt.Sprintf(":%s", flagListenPort)),
-	)
+	e.Logger.Fatal(e.Start(":8080"))
 }
